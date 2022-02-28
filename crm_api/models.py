d038e7b8ec2b1fce3ee5f779641135acdee10313
maticha84 <maticha84@gmail.com>
2022-02-28 11:51:22 +0100

    protect deletion of support or sels contact.

--- crm_api/models.py
+++ crm_api/models.py
@@ -31,7 +31,7 @@ class Client(models.Model):
     phone = models.CharField(max_length=20, verbose_name="Phone Number")
     mobile = models.CharField(max_length=20, verbose_name="Mobile Number")
     company = models.ForeignKey(to=Company, on_delete=models.RESTRICT, related_name='client_company')
-    sales_contact = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, null=True,
+    sales_contact = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, null=True,
                                       blank=True, limit_choices_to={'user_team': 3}, related_name='sales')
     date_created = models.DateTimeField(auto_now_add=True, verbose_name='Date Created')
     date_updated = models.DateTimeField(auto_now=True, verbose_name='Date Updated')
@@ -57,7 +57,7 @@ class Contract(models.Model):
     status = models.BooleanField(default=False, verbose_name="Active contract")
     amount = models.DecimalField(max_digits=10, decimal_places=2, blank=True, verbose_name="Amount")
     payment_due = models.DateTimeField(blank=True, null=True, verbose_name="Payment Due")
-    sales_contact = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, null=True,
+    sales_contact = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, null=True,
                                       blank=True, limit_choices_to={'user_team': 3}, related_name='sales_contract')
     client = models.ForeignKey(to=Client, on_delete=models.RESTRICT, related_name='client_contract')
 
@@ -85,7 +85,7 @@ class Event(models.Model):
     )
     status = models.PositiveSmallIntegerField(choices=CHOICES_STATUS, verbose_name="Status", default=1)
     contract = models.OneToOneField(to=Contract, on_delete=models.RESTRICT, related_name='contract_event')
-    support_contact = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, null=True,
+    support_contact = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.RESTRICT, null=True,
                                         blank=True, limit_choices_to={'user_team': 2}, related_name="support")
     date_created = models.DateTimeField(auto_now_add=True, verbose_name='Date Created')
     date_updated = models.DateTimeField(auto_now=True, verbose_name='Date Updated')
